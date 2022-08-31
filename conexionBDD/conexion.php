fdd14fdbd8a0509a800f4b7a2494fa674493cb68
Alexander <kevin05alexander048@gmail.com>
2022-08-31 00:02:56 -0700

    codigoFuncional

--- conexionBDD/conexion.php
+++ conexionBDD/conexion.php
@@ -1,22 +1,16 @@
-<?php
-
-class conexionBDD{
-    public static function conectarBDD(){
-        define('servidor','localhost');
-        define('nombre_db','curso_data_tables');
-        define('usuario','root');
-        define('password','');
-
-        $opciones=array(PDO::MYSQL_ATTR_INIT_COMMAND=>'SET NAMES utf8');
+<?php 
+class Conexion{	  
+    public static function Conectar() {        
+        define('servidor', 'localhost');
+        define('nombre_bd', 'prueba-datatables');
+        define('usuario', 'root');
+        define('password', '');
+        $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
         try{
-            $conexion=new PDO('mysql:host='.servidor.'; dbname='.nombre_db, usuario, password, $opciones);
+            $conexion = new PDO("mysql:host=".servidor."; dbname=".nombre_bd, usuario, password, $opciones);			
             return $conexion;
-        }catch(Exception $e){
-            die('Error de conexion:'.$e->getMessage());
+        }catch (Exception $e){
+            die("El error de Conexión es: ". $e->getMessage());
         }
     }
-
 }
-
-
-?>
